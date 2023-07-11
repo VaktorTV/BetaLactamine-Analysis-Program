4379b15ba2cf31642bfcb49a52e82c1ab3dd094f
Vaktor <vaktortvpro@gmail.com>
2023-07-11 09:58:32 +0200

    Fix R app and clean repo

--- menu.R
+++
@@ -1,7 +0,0 @@
-library(shiny)
-
-# Menu
-menu <- function()
-{
-  navbarPage(title = "DOSAGE D'ANTI-INFECTIEUX", tabPanel("Analyse"), tabPanel ("Informations"))
-}
--- ui.R
+++ ui.R
@@ -1,34 +1,6 @@
 # INCLUDE SHINY LIBRARY
 library(shiny)
-# INCLUDE APP FILES
-source("menu.R")
 
 # DEFINE UI PAGE
-ui <- fluidPage(
-  
-  menu(),
-  
-  # APP TITLE
-  titlePanel
-  (h1("DOSAGE D'ANTI-INFECTIEUX", align = "center")),
+ui <- fluidPage(theme = "form.css", includeScript(path = "form.js"), includeHTML("form.html"))
 
-  sidebarLayout
-  (
-    position="left",
-
-    # PATIENT INFORMATION PANEL
-    sidebarPanel
-    (
-      titlePanel(h1("INFORMATIONS DU PATIENT", align = "center")),
-      includeHTML("form.html"),
-      tags$script(src="form.js"),
-    ),
-    
-    # QUESTIONS AND ANALYSIS PANEL
-    mainPanel
-    (
-      titlePanel(h1("ANALYSE", align = "center")),
-      includeHTML("output.html"),
-    )
-  )
-)
