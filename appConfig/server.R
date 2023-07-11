b1d033679875ff78c46023f6b0dffcab48dd5e5e
Vaktor <vaktortvpro@gmail.com>
2023-07-11 15:39:26 +0200

    Cefepime Final Release

--- appConfig/server.R
+++
@@ -1,6 +0,0 @@
-library(shiny)
-
-# Define server logic
-server <- function(input, output) {
-  
-}
--- appConfig/ui.R
+++
@@ -1,6 +0,0 @@
-# INCLUDE SHINY LIBRARY
-library(shiny)
-
-# DEFINE UI PAGE
-ui <- fluidPage(theme = "form.css", includeScript(path = "form.js"), includeHTML("form.html"))
-
